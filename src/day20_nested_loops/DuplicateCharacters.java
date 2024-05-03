09a89d923b35a8465eb36380fac0f0a7173df662
gedigogitidze <164251359+gedigogitidze@users.noreply.github.com>
2024-05-02 22:11:18 -0400

    reviewed the nesterd loop, break and continue in nested loop

---
+++ src/day20_nested_loops/DuplicateCharacters.java
@@ -0,0 +1,35 @@
+package day20_nested_loops;
+
+public class DuplicateCharacters {
+    public static void main(String[] args) {
+
+        String str = "appleee"; // p,e
+        String checked = "";
+        for (int i = 0; i < str.length(); i++) {
+
+            char each1 = str.charAt(i);
+
+            if (checked.contains(each1+ "")){
+                continue;
+            }
+
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+
+                char each2 = str.charAt(j);
+
+                if (each1 == each2){
+                    count++;
+                }
+
+            }
+            if (count>= 2 ){
+                System.out.println(each1);
+            }
+            checked+=each1;
+
+
+        }
+
+    }
+}
