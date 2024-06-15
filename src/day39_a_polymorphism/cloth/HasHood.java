7d741cdb623606e199a5ea0670fbdc66ea13a7bf
gedigogitidze <164251359+gedigogitidze@users.noreply.github.com>
2024-06-15 15:02:33 -0400

    casting, instanceof, polymorphic array

---
+++ src/day39_a_polymorphism/cloth/HasHood.java
@@ -0,0 +1,9 @@
+package day39_a_polymorphism.cloth;
+
+public interface HasHood {
+    public abstract void putOnHood();
+
+    //public static final String NAME = "Test";
+
+    // by default if your method has no body in interface it is pubic abstract
+}
